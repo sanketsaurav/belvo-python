c3f58ebf3eae1eeb161ff5e2293aa1faabd6d780
Aristobulo Meneses <aristobulo@gmail.com>
2019-11-28 13:37:49 +0100

    Update gh-pages

--- scripts/bump-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if ! [ -x "$(command -v bumpversion)" ]; then
-  echo -e "bumpversion is required, to install it run 'pip install bumpversion'. Aborting." >&2
-  exit 1
-fi
-
-bumpversion "$1"
